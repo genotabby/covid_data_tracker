3ee4dfab50f49618f36442285e616891d0f576c1
genotabby <teohwesley@gmail.com>
2023-06-09 01:43:27 +0800

    Added addCommas function for number visibility

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,10 @@
 export default function Home({ props }) {
+  function addCommas(amount) {
+    let a = Math.trunc(amount / 1000000);
+    let b = Math.trunc((amount - a * 1000000) / 1000);
+    let c = Math.trunc(amount % 1000);
+    return `${a},${b},${c}`;
+  }
   return (
     <>
       <h1 className="Header">Global details</h1>
@@ -6,13 +12,13 @@ export default function Home({ props }) {
         Global Confirmed Cases:{" "}
         {props.length === 0
           ? "Data is loading..."
-          : props?.summaryStats?.global?.confirmed}
+          : addCommas(props?.summaryStats?.global?.confirmed)}
       </p>
       <p>
         Global Confirmed Deaths:{" "}
         {props.length === 0
           ? "Data is loading..."
-          : props?.summaryStats?.global?.deaths}
+          : addCommas(props?.summaryStats?.global?.deaths)}
       </p>
 
       {/* <p>Global Confirmed cases: {props?.summaryStats?.global?.confirmed}</p>
