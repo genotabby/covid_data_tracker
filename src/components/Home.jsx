ec098a4576f7dfc590545ef8951663baa8c5bcd3
genotabby <teohwesley@gmail.com>
2023-06-09 01:05:19 +0800

    Added loading message when still fetching

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,9 +3,20 @@ export default function Home({ props }) {
     <>
       <h1 className="Header">Global details</h1>
       <p>
-        Global Confirmed cases Home: {props?.summaryStats?.global?.confirmed}
+        Global Confirmed Cases:{" "}
+        {props.length === 0
+          ? "Data is loading..."
+          : props?.summaryStats?.global?.confirmed}
       </p>
-      <p>Global Deaths Deaths: {props?.summaryStats?.global?.deaths}</p>
+      <p>
+        Global Confirmed Deaths:{" "}
+        {props.length === 0
+          ? "Data is loading..."
+          : props?.summaryStats?.global?.deaths}
+      </p>
+
+      {/* <p>Global Confirmed cases: {props?.summaryStats?.global?.confirmed}</p>
+      <p>Global Deaths Deaths: {props?.summaryStats?.global?.deaths}</p> */}
       <h2>End of homepage</h2>
     </>
   );
