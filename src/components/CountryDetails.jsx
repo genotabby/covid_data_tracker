e42aff88b0e8124879553bcd5a30dcdd8d600943
genotabby <teohwesley@gmail.com>
2023-06-10 18:31:36 +0800

    Added more info in details

--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -24,11 +24,16 @@ export default function CountryDetails({ props }) {
     <>
       <br />
       <button onClick={handleClick}>Return</button>
-      <h1>ID: {id}</h1>
+      {/* <h1>ID: {id}</h1> */}
       <h2>
         Detail for {countryList[id]?.country},{" "}
         {countryList[id]?.countryInfo?.iso3}
       </h2>
+      <h3>
+        Coordinates: <br />
+        Latitude: {countryList[id]?.countryInfo?.lat} Longitude:{" "}
+        {countryList[id]?.countryInfo?.long}
+      </h3>
       <img
         className="image"
         width="30%"
@@ -38,14 +43,21 @@ export default function CountryDetails({ props }) {
         Details - {id} Component {JSON.stringify(country)}
       </p> */}
       <p>Population: {addCommas(countryList[id]?.population)}</p>
+      <p>Tests done: {addCommas(countryList[id]?.tests)}</p>
       <p>Cases: {addCommas(countryList[id]?.cases)}</p>
       <p>Critical: {addCommas(JSON.stringify(countryList[id]?.critical))}</p>
       <p>Deaths: {addCommas(countryList[id]?.deaths)}</p>
       <p>Recovered: {addCommas(countryList[id]?.recovered)}</p>
-      <button onClick={handleClick}>Return</button>
+      <p>Cases per Million: {addCommas(countryList[id]?.casesPerOneMillion)}</p>
       <p>
-        Details - {id} Component {JSON.stringify(countryList[id])}
+        Deaths per Million: {addCommas(countryList[id]?.deathsPerOneMillion)}
       </p>
+      <p>
+        Recovered per Million:{" "}
+        {addCommas(countryList[id]?.recoveredPerOneMillion)}
+      </p>
+      <button onClick={handleClick}>Return</button>
+      <p>{/* Details - {id} Component {JSON.stringify(countryList[id])} */}</p>
     </>
   );
 }
