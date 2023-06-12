094a5164a9f5a6f571526d31e87e74209a72cd25
genotabby <teohwesley@gmail.com>
2023-06-12 13:57:56 +0800

    Tried out victory library example

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import addCommas from "../functions/addCommas";
+import { VictoryAxis, VictoryBar, VictoryChart } from "victory";
 
 export default function Home({ props }) {
   const navigate = useNavigate();
@@ -7,6 +8,13 @@ export default function Home({ props }) {
     console.log("Click");
     navigate("/comparator");
   };
+  const data = [
+    { quarter: 1, earnings: 13000 },
+    { quarter: 2, earnings: 16500 },
+    { quarter: 3, earnings: 14250 },
+    { quarter: 4, earnings: 19000 },
+  ];
+
   return (
     <>
       <h1 className="Header">Global details</h1>
@@ -28,6 +36,26 @@ export default function Home({ props }) {
       {/* <p>Global Confirmed cases: {props?.summaryStats?.global?.confirmed}</p>
       <p>Global Deaths Deaths: {props?.summaryStats?.global?.deaths}</p> */}
       <p>TODOS: To insert top 10 cases chart here</p>
+
+      <VictoryChart
+        // domainPadding will add space to each side of VictoryBar to
+        // prevent it from overlapping the axis
+        domainPadding={20}
+      >
+        <VictoryAxis
+          // tickValues specifies both the number of ticks and where
+          // they are placed on the axis
+          tickValues={[1, 2, 3, 4]}
+          tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
+        />
+        <VictoryAxis
+          dependentAxis
+          // tickFormat specifies how ticks should be displayed
+          tickFormat={(x) => `$${x / 1000}k`}
+        />
+        <VictoryBar data={data} x="quarter" y="earnings" />
+      </VictoryChart>
+
       <h2>End of homepage</h2>
     </>
   );
