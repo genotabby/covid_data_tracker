7794df0f25d2e88c4ff87dd93e7eb9b228c92761
genotabby <teohwesley@gmail.com>
2023-06-14 12:41:38 +0800

    Try to remove breadcrumb from countryDetails

--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -52,42 +52,7 @@ export default function CountryDetails({ props }) {
   return (
     <>
       <CountryDetailsBreadcrumb id={id} />
-      {/*Breadcrumbs reference: https://www.makeuseof.com/create-breadcrumbs-in-reactjs/ */}
-      <nav>
-        <NavLink
-          to="/"
-          className={
-            location.pathname === "/"
-              ? "breadcrumb-active"
-              : "breadcrumb-not-active"
-          }
-        >
-          Home
-        </NavLink>
-        <span className="breadcrumb-arrow">&gt;</span>
-        <NavLink
-          to="/comparator"
-          className={
-            // location.pathname.startsWith("/comparator")
-            location.pathname === "/comparator"
-              ? "breadcrumb-active"
-              : "breadcrumb-not-active"
-          }
-        >
-          Comparator
-        </NavLink>
-        <span className="breadcrumb-arrow">&gt;</span>
-        <NavLink
-          to="/comparator/country/0"
-          className={
-            location.pathname === `/comparator/country/${id}`
-              ? "breadcrumb-active"
-              : "breadcrumb-not-active"
-          }
-        >
-          Country
-        </NavLink>
-      </nav>
+
       <br />
       <button onClick={handleClick}>Return</button>
 
